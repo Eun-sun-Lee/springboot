8249b9d58fe71910d673d1492a46181423b0b272
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-15 16:46:29 +0900

    회원가입 page 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,13 +2,17 @@ package hello.hellospring.controller;
 
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MemberController {
-    private final MemberService memberService;
+    private MemberService memberService;
     @Autowired //*****1. 생성자 주입
     public MemberController(MemberService memberService){
         this.memberService=memberService;
     } //스프링이 컨테이너에 있는 memberService를 가져다가 연결 시켜줌.
+
 
     //*****2. 필드 주입 -> @Autowired private MemberService memberservice;
     //******3. setter ->
@@ -20,4 +24,8 @@ public class MemberController {
 
     //여러개의 instance를 생성할 필요 없이 하나의 instance만 생성해서 공유하고 쓰면 됨.
     //스프링 컨테이너에 등록해서 쓰면 됨.
+    @GetMapping(value="/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 }
