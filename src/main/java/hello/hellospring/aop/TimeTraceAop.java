ea2d5906d6336d1d5aba5cf22d7d85016c21773d
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-22 02:27:33 +0900

    AOP 적용해야 하는 이유

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect //AOP
+@Component
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))") //패키지 하위에 전부 적용
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start= System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed(); //inline함수, 다음 method로 진행
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("END: " + joinPoint.toString() + " "+ timeMs + "ms");
+        }
+
+    }
+}
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-@SpringBootTes t
+@SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
     @Autowired MemberService memberService;
